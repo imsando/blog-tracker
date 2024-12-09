9bfdc4b3a956060f618ae1103f89aea86b85ba7e
ssddo <ujm0524@naver.com>
2024-12-09 22:17:31 +0900

    초기설정

---
+++ src/main/java/blogTracker/blogTracker/BlogTrackerApplication.java
@@ -0,0 +1,13 @@
+package blogTracker.blogTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTrackerApplication.class, args);
+	}
+
+}
