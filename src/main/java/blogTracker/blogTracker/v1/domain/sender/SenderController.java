60bcf58b2243ea4bf81258e2ee6597a6c0630ba9
ssddo <ujm0524@naver.com>
2024-12-10 23:27:45 +0900

    [feature] 일부 설정 추가

--- src/main/java/blogTracker/blogTracker/v1/domain/sender/SenderController.java
+++ src/main/java/blogTracker/blogTracker/v1/domain/sender/SenderController.java
@@ -1,4 +1,9 @@
 package blogTracker.blogTracker.v1.domain.sender;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class SenderController {
 }
