a0b13ef742bea8f2f9b85cd21e0a037154838662
ssddo <ujm0524@naver.com>
2025-01-26 17:24:30 +0900

    [f

--- src/main/java/blogTracker/blogTracker/v1/domain/sender/platform/TistoryService.java
+++ src/main/java/blogTracker/blogTracker/v1/domain/sender/platform/TistoryService.java
@@ -87,7 +87,7 @@ public class TistoryService {
             return false;
         }
     }
-    // NOTE : 2주 이내에 작성된 글인지 확인
+
     private boolean isRecent(String pubDate) {
         try {
             LocalDateTime postDate = LocalDateTime.parse(
